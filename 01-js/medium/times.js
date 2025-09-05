840dbbd541503213f089645fbf5fec1edbb27117
itsUdit24 <uditpradhan.official@gmail.com>
2025-09-05 20:25:10 +0530

    Refactor calculateTime function to improve clarity and accuracy in time measurement

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,32 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
+/*
 function calculateTime(n) {
     return 0.01;
+}
+    */
+/*
+function calculateTime(n) {
+  const startTime = Date.now(); // start in ms
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  const endTime = Date.now(); // end in ms
+  const timeTaken = (endTime - startTime) / 1000; // convert ms to seconds
+  return timeTaken; // returns seconds
+}
+*/
+
+//Tushar's approach
+function calculateTime(n) {
+  const startTime = new Date().getTime();
+  let endNumber = 0;
+  for (let index = 0; index <= n; index++) {
+    endNumber = endNumber + index;
+  }
+  const endTime = new Date().getTime();
+  return `${endTime - startTime} milliseconds`;
+
 }
